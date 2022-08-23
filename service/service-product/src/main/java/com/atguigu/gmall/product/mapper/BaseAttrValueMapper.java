a93eac8f2fd3c4a99d93beae03862fdf4ac78bb4
LY <495734113@qq.com>
2022-08-23 16:35:49 +0800

    完成一级二级三级查询

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/mapper/BaseAttrValueMapper.java
@@ -0,0 +1,18 @@
+package com.atguigu.gmall.product.mapper;
+
+import com.atguigu.gmall.model.product.BaseAttrValue;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author mi
+* @description 针对表【base_attr_value(属性值表)】的数据库操作Mapper
+* @createDate 2022-08-23 10:20:21
+* @Entity com.atguigu.gmall.product.domain.BaseAttrValue
+*/
+public interface BaseAttrValueMapper extends BaseMapper<BaseAttrValue> {
+
+}
+
+
+
+
---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/BaseAttrInfoService.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.product.service;
+
+import com.atguigu.gmall.model.product.BaseAttrInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author mi
+* @description 针对表【base_attr_info(属性表)】的数据库操作Service
+* @createDate 2022-08-23 10:20:21
+*/
+public interface BaseAttrInfoService extends IService<BaseAttrInfo> {
+
+}
