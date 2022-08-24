ff6dfc559382b50697841f983511e3ef143be117
LY <495734113@qq.com>
2022-08-24 16:33:15 +0800

    完成品牌列表分页查询

--- service/service-product/src/main/java/com/atguigu/gmall/product/controller/BaseTrademarkController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/BaseTrademarkController.java
@@ -3,12 +3,10 @@ package com.atguigu.gmall.product.controller;
 import com.atguigu.gmall.common.result.Result;
 import com.atguigu.gmall.model.product.BaseTrademark;
 import com.atguigu.gmall.product.service.BaseTrademarkService;
+import com.baomidou.mybatisplus.extension.api.R;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 品牌列表
@@ -19,6 +17,13 @@ public class BaseTrademarkController {
 
     @Autowired
     BaseTrademarkService baseTrademarkService;
+
+    /**
+     * 分页查询品牌列表
+     * @param pn
+     * @param ps
+     * @return
+     */
     //baseTrademark/1/10
     @GetMapping("/baseTrademark/{pn}/{ps}")
     public Result getBaseTrademark(@PathVariable("pn")Long pn,
@@ -26,5 +31,50 @@ public class BaseTrademarkController {
         Page<BaseTrademark> pageResult =  baseTrademarkService.page(new Page<>(pn,ps));
         return Result.ok(pageResult);
     }
+    ///baseTrademark/save  添加品牌
+
+    /**
+     * 添加品牌
+     * @param baseTrademark
+     * @return
+     */
+    @PostMapping("baseTrademark/save")
+    public Result save(@RequestBody BaseTrademark baseTrademark){
+        baseTrademarkService.save(baseTrademark);
+        return Result.ok();
+    }
+    //baseTrademark/get/13
+
+    /**
+     * 通过id查询品牌信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/baseTrademark/get/{id}")
+    public Result getById(@PathVariable("id")Long id){
+        BaseTrademark baseTrademark = baseTrademarkService.getById(id);
+        return Result.ok(baseTrademark);
+    }
+
+    //baseTrademark/update
+
+    /**
+     * 修改品牌信息
+     * @param baseTrademark
+     * @return
+     */
+    @PutMapping("/baseTrademark/update")
+    public Result update(@RequestBody BaseTrademark baseTrademark){
+        baseTrademarkService.updateById(baseTrademark);
+        return Result.ok();
+    }
+    //baseTrademark/remove/12
+    @DeleteMapping("/baseTrademark/remove/{id}")
+    public Result delete(@PathVariable("id")Long id){
+        baseTrademarkService.removeById(id);
+        return Result.ok();
+    }
+
+
 
 }
